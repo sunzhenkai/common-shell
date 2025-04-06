8947404e5afbfd59cfecb1f58fc7965adcdc9f4c
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-06 16:24:24 +0800

    enhance docker build

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -11,20 +11,25 @@ function docker::ensure_container() {
 }
 
 function docker::build() {
-  usage="docker::build {build-image} {source-path} {build-script} {dockerfile} {dest-image-tag}"
+  usage="docker::build {build-image} {source-path} {build-script} [{dockerfile} {dest-image-tag}]"
   local build_image=$1
   local source_path=$2
   local build_script=$3
   local dockerfile=$4
   local dest_image_tag=$5
-  [[ $# -ne 5 ]] && ERROR "unexpected params count. \n\tparams($#): $@ \n\tusage: $usage" && return 5
+  [[ $# -lt 3 ]] && ERROR "unexpected params count. \n\tparams($#): $@ \n\tusage: $usage" && return 5
 
+  # build application
   local c_build_dir='/build'
   docker run --rm -t \
     -v "${source_path}:${c_build_dir}" \
     ${build_image} \
-    bash -c "cd ${c_build_dir} && bash ${build_script}"
-  [[ $? -eq 0 ]] && ERROR "build application failed" && return 5
-  docker build -f ${dockerfile} -t ${dest_image_tag} ${c_build_dir}
-  [[ $? -eq 0 ]] && ERROR "build image failed" && return 5
+    bash --login -c "cd ${c_build_dir} && bash ${build_script}"
+  [[ $? -ne 0 ]] && ERROR "build application failed" && return 5
+
+  # [optional] build application image
+  if [[ $# -eq 5 ]]; then
+    docker build -f ${dockerfile} -t ${dest_image_tag} ${c_build_dir}
+    [[ $? -eq 0 ]] && ERROR "build image failed" && return 5
+  fi
 }
